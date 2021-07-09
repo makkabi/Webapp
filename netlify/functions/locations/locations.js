aadd946bc978a55d27c1d06843052a3020600921
makkabi <j.weckerle@jpberlin.de>
2021-07-09 12:00:55 +0200

    Add json header

--- netlify/functions/locations/locations.js
+++ netlify/functions/locations/locations.js
@@ -7,6 +7,9 @@ exports.handler = async function (event, context) {
 
   return {
     statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify(locations),
   };
 };
